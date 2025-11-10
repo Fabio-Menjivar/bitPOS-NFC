a9bb9b5cca316bc74a8a51a5e47ac0a5f9f3fd67
arthur <arthureli81@gmail.com>
2025-11-09 18:03:25 -0600

    agregar prisma al proyecto y crear tabla users

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express, {Request, Response} from 'express';
 
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
